1d734790367da6325710a6072dd6d878ed52c36d
J.E. Omole <joshdavidson.oj@gmail.com>
2022-06-28 22:33:11 +0100

    print fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i == 100:
-            print("{}".format("Buzz"), end=" \n")
-        elif ((i % 15) == 0):
+        if ((i % 15) == 0):
             print("{}".format("FizzBuzz"), end=" ")
         elif ((i % 5) == 0):
             print("{}".format("Buzz"), end=" ")
