8d78f310a78dfc46478b5ce177385727e635e0f1
J.E. Omole <joshdavidson.oj@gmail.com>
2022-07-06 20:38:58 +0100

    square all values in a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,5 @@
 def square_matrix_simple(matrix=[]):
     squared = []
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            squared.append(list(map((lambda x: x**2), matrix[i])))
-            break
+        squared = [(list(map((lambda x: x**2), matrix[i])))]
     return squared
